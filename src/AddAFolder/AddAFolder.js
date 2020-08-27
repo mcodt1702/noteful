894539d4625ce3a4e2f30293a8b01e06eec7a6f2
mcodt1702 <marco@mcnyc.net>
2020-08-27 10:18:27 -0400

    validation error

--- src/AddAFolder/AddAFolder.js
+++ src/AddAFolder/AddAFolder.js
@@ -1,9 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
 import Context from ".././Context";
+import ValidationError from "../ValidationError/ValidationError";
 
 export default class AddAFolder extends React.Component {
   static contextType = Context;
+
+  constructor() {
+    super();
+
+    this.state = {
+      name: "",
+      touched: false,
+    };
+  }
+
+  nameUpdate(name) {
+    this.setState({ name: { value: name, touched: true } });
+  }
+  validateName() {
+    const name = this.state.name.value.trim();
+    if (name.length === 0) {
+      return "Name is required";
+    } else if (name.length < 4) {
+      return "You need at least 3 characters";
+    }
+  }
 
   render() {
     return (
@@ -16,7 +38,13 @@ export default class AddAFolder extends React.Component {
             name="title"
             placeholder="Title"
             aria-label="Folder Title"
+            value={this.state.name.value}
+            onChange={(e) => this.nameUpdate(e.target.value)}
+            required
           />
+          {this.state.name.touched && (
+            <ValidationError message={this.validateName()} />
+          )}
           <input
             type="submit"
             name="submit"
